3ae8f9c0074681f485c773ddc68003b7cf6ec782
Sergey Kolodyazhnyy <skolodyazhny@magento.com>
2016-07-04 10:22:15 +0200

    Add service, structure and extension browser

--- specification/js/specification_browser.js
+++ specification/js/specification_browser.js
@@ -1,29 +1,96 @@
 var SpecBrowser = {
     specification: [],
-    load: function(url) {
+    fetch: function(url) {
         $.getJSON(url, function(specification) {
-            SpecBrowser.specification = specification;
+            SpecBrowser.load(specification);
             SpecBrowser.jump(document.location.hash.slice(1) || 'services');
         });
     },
-    jump: function(to) {
-        if (this.isTabName(to)) {
-            this.renderTab(to);
+    load: function(specification) {
+        this.specification = specification;
+        this.index = {};
+
+        for (var m in specification) {
+            if (!specification.hasOwnProperty(m)) {
+                continue;
+            }
+
+            var module = specification[m].module;
+            var services = specification[m].services || [];
+            var structures = specification[m].structures || [];
+            var extensions = specification[m].extensions || [];
+
+            for (var si = 0; si < services.length; si++) {
+                this.index[module + "." + services[si].name] = {
+                    module: module,
+                    type: 'service',
+                    section: 'services',
+                    document: services[si]
+                };
+            }
+
+            for (var ti = 0; ti < structures.length; ti++) {
+                this.index[module + "." + structures[ti].name] = {
+                    module: module,
+                    type: 'structure',
+                    section: 'structures',
+                    document: structures[ti]
+                };
+            }
+
+            for (var ei = 0; ei < extensions.length; ei++) {
+                this.index[module + "." + extensions[ei].extends + "." + extensions[ei].name] = {
+                    module: module,
+                    type: 'extension',
+                    section: 'extensions',
+                    document: extensions[ei]
+                };
+            }
         }
     },
-    isTabName: function(name) {
-        return ['services', 'structures', 'extensions'].indexOf(name) !== false;
-    },
-    renderTab: function(tab) {
-        $(".page-tab-bar a[data-active-tab]").removeClass('active');
-        $(".page-tab-bar a[data-active-tab=" + tab.replace( /(:|\.|\[|\]|,)/g, "\\$1" ) +"]").addClass('active');
+    jump: function(to) {
+        if (this.isSectionName(to)) {
+            return this.renderIndex(to);
+        }
 
-        this.render(tab + "-navigation", "#navbar");
-        this.render(tab + "-page", "#view");
+        if (this.isElementName(to)) {
+            return this.renderElement(to);
+        }
+
+        return this.jump('services');
     },
-    render: function(view, where) {
+    isSectionName: function(name) {
+        return ['services', 'structures', 'extensions'].indexOf(name) != -1;
+    },
+    isElementName: function(name) {
+        return this.index.hasOwnProperty(name);
+    },
+    renderLayout: function(layout) {
+        $(".page-tab-bar a[data-active-tab]").removeClass('active');
+        $(".page-tab-bar a[data-active-tab=" + layout.replace( /(:|\.|\[|\]|,)/g, "\\$1" ) +"]").addClass('active');
+
+        this.render(layout + "-navigation", "#navbar");
+    },
+    renderIndex: function(section) {
+        this.renderLayout(section);
+        this.render(section + "-index", "#view");
+    },
+    renderElement: function(name) {
+        var element = this.index[name];
+
+        this.renderLayout(element.section);
+        this.render(element.section + "-page", "#view", {
+            element: element.document,
+            module: element.module
+        });
+    },
+    render: function(view, where, args) {
         var source   = $("#"+view+"-template").html();
         var template = Handlebars.compile(source);
-        $(where).html(template({specification: this.specification}));
+        var context = args || {};
+
+        context.specification = this.specification;
+
+        $(where).html(template(context));
     }
 };
